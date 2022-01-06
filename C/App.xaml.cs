ca4f4880c857f4f48dbd1e0c0ee9344d77f129e4
Nguyễn Sĩ Liêm <ngggslim@gmail.com>
2022-01-06 21:43:40 +0700

    Add project files.

---
+++ C/App.xaml.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Drawing.Imaging;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace C
+{
+    static class Constant
+    {
+        public const int MAX_FAILED_COUNT = 3; //in seconds
+        public const int TIME_BEFORE_USE = 3; //in seconds
+        public const int PARENT_USE_DURATION = 60 * 60 * 1000; //60 minutes, in milliseconds
+        public const int ONE_SECOND = 1000; //in miliseconds
+        public const int WRONG_PASSWORD_SHUTDOWN = 10 * 60; //10 minutes, in seconds
+        public const int WRONG_PHASE_SHUTDOWN = 15; //15 seconds
+        public const int REFRESH_RATE = 1 * 60 * 1000; //1 minute, in milliseconds
+    }
+
+    public class Time
+    {
+        public int Hour;
+        public int Minute;
+
+        public Time()
+        {
+            Hour = 0;
+            Minute = 0;
+        }
+
+        public Time(int _hour, int _minute)
+        {
+            Hour = _hour;
+            Minute = _minute;
+        }
+
+        public Time(string timeString) //format: hh:mm
+        {
+            string[] components = timeString.Split(':');
+            Hour = Int32.Parse(components[0]);
+            Minute = Int32.Parse(components[1]);
+        }
+
+        public static bool operator ==(Time a, Time b)
+        {
+            return a.Hour == b.Hour && a.Minute == b.Minute;
+        }
+
+        public static bool operator !=(Time a, Time b)
+        {
+            return a.Hour != b.Hour || a.Minute != b.Minute;
+        }
+
+        public static bool operator >(Time a, Time b)
+        {
+            return a.Hour > b.Hour || ((a.Hour == b.Hour) && (a.Minute > b.Minute));
+        }
+
+        public static bool operator <(Time a, Time b)
+        {
+            return a.Hour < b.Hour || ((a.Hour == b.Hour) && (a.Minute < b.Minute));
+        }
+
+        public static bool operator <=(Time a, Time b)
+        {
+            return a < b || a == b;
+        }
+
+        public static bool operator >=(Time a, Time b)
+        {
+            return a > b || a == b;
+        }
+
+        public static Time operator +(Time a, Time b)
+        {
+            Time sum = new Time();
+            sum.Hour = a.Hour + b.Hour;
+            sum.Hour += (a.Minute + b.Minute)/ 60;
+            sum.Minute = (a.Minute + b.Minute) % 60;
+            return sum;
+        }
+
+        public static Time operator -(Time a, Time b)
+        {
+            Time sub = new Time();
+            sub.Hour = a.Hour - b.Hour;
+            if(a.Minute >= b.Minute)
+            {
+                sub.Minute = a.Minute - b.Minute;
+            } else
+            {
+                sub.Hour -= 1;
+                sub.Minute = a.Minute - b.Minute + 60;
+            }
+            return sub;
+        }
+
+        public static Time Now()
+        {
+            DateTime dateTime = DateTime.Now;
+            return new Time(dateTime.Hour, dateTime.Minute);
+        }
+
+        public int ToSeconds()
+        {
+            return Hour * 3600 + Minute * 60;
+        }
+
+        public int ToMinutes()
+        {
+            return Hour * 60 + Minute;
+        }
+
+        public static Time MinuteToTime(int minute)
+        {
+            return new Time(minute/60, minute%60);
+        }
+
+        public override string ToString()
+        {
+            string timeString = "";
+            if (Hour < 10)
+            {
+                timeString += "0" + Hour.ToString();
+            }
+            else
+            {
+                timeString += Hour.ToString();
+            }
+            timeString += ":";
+            if (Minute < 10)
+            {
+                timeString += "0" + Minute.ToString();
+            }
+            else
+            {
+                timeString += Minute.ToString();
+            }
+            return timeString;
+        }
+    }
+
+    public class Phase
+    {
+        public Time From = new Time();
+        public Time To = new Time();
+        public int Duration = 0;
+        public int InterruptTime = 0;
+        public int Sum = 0;
+
+        public Phase(string phaseString)
+        {
+            string[] components = phaseString.Split(' ');
+            foreach (string component in components)
+            {
+                char index = component[0];
+                string value = component.Substring(1);
+                if (index == 'F')
+                {
+                    From = new Time(value); 
+                }
+                if (index == 'T')
+                {
+                    To = new Time(value);
+                }
+                if (index == 'D')
+                {
+                    Duration = Int32.Parse(value);
+                }
+                if (index == 'I')
+                {
+                    InterruptTime = Int32.Parse(value);
+                }
+                if (index == 'S')
+                {
+                    Sum = Int32.Parse(value);
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            string phaseString = "";
+            if (Duration != 0)
+            {
+                phaseString += "\nThời gian mỗi quãng: " + Duration.ToString() + " phút";
+            }
+            if (InterruptTime != 0)
+            {
+                phaseString += "\nThời gian nghỉ giữa quãng: " + InterruptTime.ToString() + " phút";
+            }
+            if (Sum != 0)
+            {
+                phaseString += "\nTổng thời gian được sử dụng: " + Sum.ToString() + " phút";
+            }
+            return phaseString;
+        }
+
+        public bool hasTime(Time time)
+        {   
+            if (time >= From && time <= To)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+
+    public static class Utils
+    {
+        public static void CaptureScreen()
+        {
+            Bitmap captureBitmap = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height, PixelFormat.Format32bppArgb);
+            Rectangle captureRectangle = Screen.AllScreens[0].Bounds;
+            Graphics captureGraphics = Graphics.FromImage(captureBitmap);
+            captureGraphics.CopyFromScreen(captureRectangle.Left, captureRectangle.Top, 0, 0, captureRectangle.Size);
+            DateTime now = DateTime.Now;
+            string fileName = $"Screenshot_{now.Day.ToString()}_{now.Month.ToString()}_{now.Year.ToString()}_{now.Hour.ToString()}h{now.Minute.ToString()}m{now.Second.ToString()}s";
+            captureBitmap.Save(@$"C:\Users\Gambon\Desktop\{fileName}.jpg", ImageFormat.Jpeg);
+        }
+    }
+
+    public partial class App : System.Windows.Application
+    {
+
+    }
+}
---
+++ C/MainWindow.xaml.cs
@@ -0,0 +1,359 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+
+
+namespace C
+{
+    public partial class MainWindow : Window
+    {
+        String parentPassword = "admin";
+        String childrenPassword = "children";
+        bool hasScheduledThread;
+        int failedCount = 0;
+        List<Phase> phaseList;
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Topmost = true;
+            txtPassword.Focus();
+            Utils.CaptureScreen();
+            phaseList = readPhaseList();
+            if (phaseList.Count == 0)
+            {
+                displayAlertBox("Không có khung giờ nào được sử dụng", "Lỗi");
+                return;
+            }    
+        }
+
+        private void btnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            handleLogin();
+        }
+
+        private void txtPassword_Submit(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.Return)
+            {
+                handleLogin();
+            }
+        }
+
+        private async void handleLogin()
+        {
+            if (txtPassword.SecurePassword.Length == 0)
+            {
+                //displayAlertBox("Chưa nhập mật khẩu");
+                txtMessage.Text = "Chưa nhập mật khẩu";
+            }
+            else if (txtPassword.Password.ToString() == childrenPassword)
+            {
+                
+                handlePostLogin();
+            }
+            else if (txtPassword.Password.ToString() == parentPassword)
+            {
+                if (hasScheduledThread)
+                {
+                    removeScheduledShutdown();
+                    hasScheduledThread = false;
+                }
+                displayAlertBox($"Phụ huynh được dùng máy trong {Constant.PARENT_USE_DURATION / (60 * 1000)} phút", "Thông báo");
+                hideWindow();
+                await Task.Delay(Constant.PARENT_USE_DURATION);
+                resetWindow();
+            }
+            else if (txtPassword.Password.ToString() == "close")
+            {
+                Close();
+            }
+            else
+            {
+                txtMessage.Text = "Sai mật khẩu";
+                txtPassword.Password = "";
+                failedCount++;
+                if (failedCount == Constant.MAX_FAILED_COUNT)
+                {
+                    hasScheduledThread = true;
+                    Thread thread = new Thread(() => setMessage($"Bạn đã sai quá {Constant.MAX_FAILED_COUNT} lần, hệ thống sẽ tắt sau {Constant.WRONG_PASSWORD_SHUTDOWN/60} phút"));
+                    thread.Start();
+                    lockLogin();
+                    setScheduledShutdown(Constant.WRONG_PASSWORD_SHUTDOWN);
+                }
+            }
+        }
+
+        private void hideWindow()
+        {
+            this.Dispatcher.Invoke(new Action(() =>
+            {
+                Hide();
+            }));
+        }
+
+        private void showWindow()
+        {
+            this.Dispatcher.Invoke(new Action(() =>
+            {
+                Show();
+            }));
+        }
+
+        private void resetWindow()
+        {
+            failedCount = 0;
+            this.Dispatcher.Invoke(new Action(() =>
+            {
+                txtPassword.Focus();
+                txtPassword.Password = "";
+                txtMessage.Text = "Phiên đăng nhập đã hết, xin vui lòng đăng nhập lại";
+                Show();
+            }));
+            this.WindowState = WindowState.Maximized;
+            unlockLogin();
+        }
+        
+        private void lockLogin()
+        {
+            this.Dispatcher.Invoke(new Action(() =>
+            {
+                txtPassword.IsEnabled = false;
+                btnSubmit.IsEnabled = false;
+            }));
+        }
+
+        private void unlockLogin()
+        {
+            this.Dispatcher.Invoke(new Action(() =>
+            {
+                txtPassword.IsEnabled = true;
+                btnSubmit.IsEnabled = true;
+            }));
+        }
+
+        protected override void OnPreviewKeyDown(KeyEventArgs e)
+        {
+            if (e.Key == Key.LWin || e.Key == Key.RWin)
+            {
+                e.Handled = true;
+                return;
+            }
+            if (e.Key == Key.System && e.SystemKey == Key.F4)
+            {
+                e.Handled = true;
+            }
+            if ((e.Key == Key.LeftAlt || e.Key == Key.RightAlt) && (e.Key == Key.LeftShift || e.Key == Key.RightShift) && e.Key == Key.Escape)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void setScheduledShutdown(int seconds)
+        {
+            Process.Start("shutdown", $"/s /t {seconds}");
+        }
+
+        private void removeScheduledShutdown()
+        {
+            Process.Start("shutdown", "/a");
+        }
+
+        private List<Phase> readPhaseList()
+        {
+            List<Phase> phaseList = new List<Phase>();
+
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\Gambon\Desktop\test.txt");
+            foreach (string line in lines)
+            {
+                Phase newPhase = new Phase(line);
+                phaseList.Add(newPhase);
+            }
+
+            return phaseList;
+        }
+
+        private void handlePostLogin()
+        {
+            bool canUse = false;
+            int phaseIndex = 0;
+
+            phaseList = readPhaseList();
+
+            foreach (Phase phase in phaseList)
+            {
+                if(phase.hasTime(Time.Now()))
+                {
+                    canUse = true;
+                    break;
+                }
+                phaseIndex++;
+            }
+            
+            if (canUse)
+            {
+                string title = $"Phiên sử dụng {phaseList[phaseIndex].From.ToString()} - {phaseList[phaseIndex].To.ToString()}";
+                MessageBox.Show("Đăng nhập thành công!\n" + phaseList[phaseIndex].ToString(), title);
+
+                handlePhase(Time.Now(), phaseIndex);
+            }
+            else
+            {
+                this.Dispatcher.Invoke(new Action(() =>
+                {
+                    phaseIndex = -1;
+
+                    for (int i = 0; i < phaseList.Count; i++)
+                    {
+                        if (phaseList[i].From > Time.Now())
+                        {
+                            if (phaseIndex == -1)
+                            {
+                                phaseIndex = i;
+                            }
+                            else
+                            {
+                                if (phaseList[i].From < phaseList[phaseIndex].From)
+                                {
+                                    phaseIndex = i;
+                                }
+                            }
+                        }
+                    }
+                    if (phaseIndex == -1)
+                    {
+                        phaseIndex = 0;
+                    }
+                    txtMessage.Text = $"Không trong thời gian được sử dụng máy, thử lại vào {phaseList[phaseIndex].From}, máy sẽ tắt sau {Constant.WRONG_PHASE_SHUTDOWN} giây nếu phụ huynh không đăng nhập";
+                    setScheduledShutdown(Constant.WRONG_PHASE_SHUTDOWN);
+                    hasScheduledThread = true;
+                }));
+            }
+        }
+
+        private async void handlePhase(Time startTime, int phaseIndex)
+        {
+            Phase phase = phaseList[phaseIndex];
+
+            if (phase.Sum != 0)
+            {
+                displayAlertBox($"Hệ thống sẽ khoá vào {phase.To.ToString()} hoặc sau {phase.Sum.ToString()} phút sử dụng", "Thông báo");
+            }
+            else
+            {
+                displayAlertBox($"Hệ thống sẽ khoá vào {phase.To.ToString()}", "Thông báo");
+            }
+
+            int timeUsed = 0;
+            int timeInPeriod = 0;
+
+            hideWindow();
+            lockLogin();
+
+            while (true)
+            {
+                Utils.CaptureScreen();
+
+                if (timeUsed >= phase.Sum || Time.Now() >= phase.To)
+                {
+                    showWindow();
+                    setMessage($"Hệ thống sẽ mở khoá sau khi kết thúc phiên hoạt động vào {phase.To}");
+                    Thread unlockLoginThread = new Thread(async () =>
+                    {
+
+                        await Task.Delay((phase.To - Time.Now()).ToSeconds() * 1000);
+                        resetWindow();
+                        return;
+                    });
+                    unlockLoginThread.Start();
+                    return;
+                }
+
+                if (timeInPeriod == phase.Duration)
+                {
+                    showWindow();
+                    setMessage($"Đã đến giờ nghỉ, bạn sẽ được tiếp tục sử dụng sau {phase.InterruptTime} phút");
+                    await Task.Delay(phase.InterruptTime * 60 * 1000);
+                    if (Time.Now()>=phase.To)
+                    {
+                        resetWindow();
+                        return;
+                    }
+                    timeInPeriod = 0;
+                    hideWindow();
+                }
+
+                if (timeUsed >= phase.Sum - 1 || Time.Now() == phase.To - new Time("00:01"))
+                {
+                    displayAlertBox($"Hệ thống sẽ khoá 1 phút nữa", "Thông báo");
+                }
+
+                await Task.Delay(Constant.REFRESH_RATE);
+                int REFRESH_CODE = refreshPhase(ref phase, phaseIndex);
+                if (REFRESH_CODE == 2)
+                {
+                    MessageBox.Show($"Phiên sử dụng đã bị xoá", "Thông báo");
+                    resetWindow();
+                    return;
+                }
+                if (REFRESH_CODE == 1)
+                {
+                    string title = $"Phiên sử dụng {phase.From.ToString()} - {phase.To.ToString()}";
+                    MessageBox.Show("Phiên đã bị thay đổi\n" + phase.ToString(), title);
+                }    
+                timeUsed++;
+                timeInPeriod++;
+            }
+        }
+
+        private int refreshPhase(ref Phase phase, int phaseIndex)
+        {
+            phaseList = readPhaseList();
+            if (phaseIndex >= phaseList.Count)
+            {
+                return 2;
+            }
+            
+            foreach (Phase _phase in phaseList)
+            {
+                if (Time.Now() >= _phase.From && Time.Now() <= _phase.To)
+                {
+                    if(phase.From != _phase.From || phase.To != _phase.To || phase.Duration != _phase.Duration || phase.InterruptTime != _phase.InterruptTime || phase.Sum != _phase.Sum)
+                    {
+                        phase = _phase;
+                        return 1; //Phase's changed
+                    }
+                    else
+                    {
+                        return 0; //Phase's unchanged
+                    }
+                }
+            }
+
+            return 2;
+        }
+
+        private void setMessage(String message)
+        {
+            this.Dispatcher.Invoke(new Action(() =>
+            {
+                txtMessage.Text = message;
+            }));
+        }
+
+
+        private void displayAlertBox(String message, String title)
+        {
+            Thread messageBoxThread = new Thread(() =>
+            {
+                MessageBox.Show(message, title);
+            });
+            messageBoxThread.Start();
+        }
+    }
+}
+
